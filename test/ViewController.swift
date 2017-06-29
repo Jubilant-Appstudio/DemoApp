2a20df67c5d30f8d3cfa1c20d501f0664988fddf
Jeny Watson <jeny.the1infotech@gmail.com>
2017-06-29 17:47:13 +0530

    changes data

---
+++ test/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Mahipalsinh on 6/29/17.
+//  Copyright © 2017 Mahipalsinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        print("hello world")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
